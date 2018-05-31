0a37b87043a5f2c15b3d70c95fee2d1dadfec811
unixpod <38276514+unixpod@users.noreply.github.com>
2018-05-30 22:21:08 -0300

    dump file test
    dumb file test

---
+++ fullvector.cpp
@@ -0,0 +1,27 @@
+//       Filename:  fullvector.cpp
+//    Description:  
+//        Version:  1.0 Rev: none Org: 
+//        Created:  30-05-2018 21:49:10
+//       Compiler:  g++  Author:  unixpod celo (), 
+#include <iostream>
+using namespace std;
+int main(){
+    int valores, par[5], impar[5], x, y, i, j;
+    for (i=0;i<16;i++){
+        cin >> valores;
+            for (x=0;x<=5;x++) {
+                if (valores % 2 == 0) {
+                    par[x]+=valores;
+                    cout << "par[" << x << "] = " << valores << endl;
+
+                }
+                if (valores % 2 == 1) {
+                    impar[x]+=valores;
+                    cout << "impar[" << x << "] = " << valores << endl;
+
+                }
+            }
+    }
+    return 0;
+}
+
