2399bd4cab832f30df2ce610aa0f0f0cdb784845
unixpod <38276514+unixpod@users.noreply.github.com>
2018-05-31 12:29:45 -0300

    Acho q agora foi
    -7 mod = -1

--- fullvector.cpp
+++ fullvector.cpp
@@ -6,28 +6,28 @@
 #include <iostream>
 using namespace std;
 int main(){
-    int valores, par[5], impar[5], x=0, y, i, j=0, z;
-    for (i=0;i<15;i++){
+    int valores, par[5], impar[5], x=0, y, i, j=0, z, q=15;
+    while (q--){
         cin >> valores;
 
             if (valores % 2 == 0) {            
 
-                    par[x]+=valores;
+                    par[x]=valores;
                     x++;            
 
                     
-                        if (x == 4) {
+                        while (x == 5) {
                             for (x=0;x<5;x++){
                                 cout << "par[" << x << "] = " << par[x]<< endl;
                             }
                             x=0;
                         }
                 }
-                if (valores % 2 == 1) {
+                if (valores % 2 == 1 || valores % 2 == -1) {
                         
-                        impar[j]+=valores;
+                        impar[j]=valores;
                         j++;
-                        if (j == 4) {
+                        while (j == 5) {
                             for (j=0;j<5;j++){
                                  cout << "impar[" << j << "] = " << impar[j] << endl;
 
@@ -37,16 +37,13 @@ int main(){
 
                 }
                 
-            }
-            for(int z =0; z<x;z++  ){
-                cout << "par[" << z << "]" << impar[z] << endl;
-
-            }
-            for (int z =0; z<j;z++){
-                cout << "impar[ =" << z << "]" << par[z] << endl;
-
-            }
-            
+    }
+    for (int z=0;z<j;z++){
+        cout << "impar[" << z << "] = " << impar[z] << endl;
+    }
+    for (int z=0;z<x;z++){
+        cout << "par[" << z << "] = " << par[z] << endl;
+    }
     return 0;
     }
 
